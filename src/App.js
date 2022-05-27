59b9bdfeef7a38a692d884bcb72cb2726d03ec5c
Nipun Thathsara <47180299+thathsaranipun@users.noreply.github.com>
2022-05-27 10:10:18 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react'
+import Button from './Components/Button';
+import PostView from './Components/PostView';
+
+function App() {
+  return (
+    <div className="App">
+      <Button />
+      <br></br>
+      <PostView />
+    </div>
+  );
+}
+
+export default App;
